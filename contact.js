de9a0de7c3e2b49f61e37e498c26a7df5b8d394a
Tutur Bytes <info.tuturbytes@gmail.com>
2026-02-16 23:05:15 +0500

    Add emailjs integration for contact form

---
+++ contact.js
@@ -0,0 +1,11 @@
+emailjs.init("YOUR_PUBLIC_KEY");
+
+document.getElementById("contact-form")
+.addEventListener("submit",function(e){
+e.preventDefault();
+
+emailjs.sendForm("YOUR_SERVICE_ID","YOUR_TEMPLATE_ID",this)
+.then(function(){
+alert("Message Sent Successfully!");
+});
+});
